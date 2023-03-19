da1187fd60cedcee9329d429c63d9d7c8489cec1
Hiroki Tokunaga <tokusan441@gmail.com>
2023-03-19 18:11:33 +0900

    Delete characters on the correct typing

--- components/display.tsx
+++ components/display.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import styles from "./display.module.css";
 
+const original = "e=2.7";
+
 export default function Display() {
+    const [text, setText] = useState(original);
+
+    useEffect(() => {
+        function onKeyDown(e: KeyboardEvent) {
+            if (e.key !== text[0]) {
+                return;
+            }
+
+            setText((text) => {
+                if (text.length === 1) {
+                    return original;
+                } else {
+                    return text.slice(1);
+                }
+            });
+        }
+
+        document.addEventListener("keydown", onKeyDown);
+
+        return () => {
+            document.removeEventListener("keydown", onKeyDown);
+        };
+    }, [text, setText]);
+
     return (
         <div className={styles.container}>
-            <div className={styles["main-text"]}>
-                e=2.718281828459045235360287471352662497757247093699959574966967627724076630353
-            </div>
+            <div className={styles["main-text"]}>{text}</div>
         </div>
     );
 }
