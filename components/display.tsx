337a9c29f353f85ac71201b3d4dff563cc54f057
Hiroki Tokunaga <tokusan441@gmail.com>
2023-03-21 16:22:28 +0900

    Specify fonts

--- components/display.tsx
+++ components/display.tsx
@@ -1,9 +1,16 @@
+import { Source_Code_Pro } from "next/font/google";
+
 import escapeSpace from "../utils/escapeSpace";
 import styles from "./display.module.css";
 
+const sourceCodePro = Source_Code_Pro({
+    weight: "400",
+    subsets: ["latin"],
+});
+
 export default function Display({ text }: { text: string }) {
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} ${sourceCodePro.className}`}>
             <div className={styles["main-text"]}>{escapeSpace(text)}</div>
         </div>
     );
