3863df06003b41a8a5fb16482d79446dd7815cb4
Hiroki Tokunaga <tokusan441@gmail.com>
2023-03-21 16:39:55 +0900

    Highlight the first character

--- components/display.tsx
+++ components/display.tsx
@@ -9,9 +9,16 @@ const sourceCodePro = Source_Code_Pro({
 });
 
 export default function Display({ text }: { text: string }) {
+    const escaped = escapeSpace(text);
+    const head = escaped[0];
+    const tail = escaped.slice(1);
+
     return (
         <div className={`${styles.container} ${sourceCodePro.className}`}>
-            <div className={styles["main-text"]}>{escapeSpace(text)}</div>
+            <div className={styles["main-text"]}>
+                <span className={styles["first-character"]}>{head}</span>
+                {tail}
+            </div>
         </div>
     );
 }
