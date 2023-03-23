b0d5b6f5b956ff88f62641566f3bd24be7c52400
Hiroki Tokunaga <tokusan441@gmail.com>
2023-03-24 00:08:57 +0900

    Split into functions

--- components/DoubleRowsKey.tsx
+++ components/DoubleRowsKey.tsx
@@ -3,28 +3,18 @@ import styles from "./keyboard.module.css";
 type DoubleRowsKeyProps = {
     readonly upper: string;
     readonly lower: string;
+    // A workaround for false-positive.
+    // eslint-disable-next-line react/no-unused-prop-types
     readonly isNextKey: boolean;
+    // eslint-disable-next-line react/no-unused-prop-types
     readonly pressed: boolean;
 };
 
-export default function DoubleRowsKey({
-    upper,
-    lower,
-    isNextKey,
-    pressed,
-}: DoubleRowsKeyProps) {
-    const classes = [styles.key];
-
-    if (pressed) {
-        classes.push(styles.typed);
-    }
-
-    if (isNextKey) {
-        classes.push(styles.next);
-    }
+export default function DoubleRowsKey(props: DoubleRowsKeyProps) {
+    const { upper, lower } = props;
 
     return (
-        <div className={classes.join(" ")}>
+        <div className={classes(props)}>
             <div className={styles["key-label"]}>
                 {upper}
                 <br />
@@ -32,4 +22,20 @@ export default function DoubleRowsKey({
             </div>
         </div>
     );
+}
+
+function classes(props: DoubleRowsKeyProps): string {
+    const { pressed, isNextKey } = props;
+
+    const l = [styles.key];
+
+    if (pressed) {
+        l.push(styles.typed);
+    }
+
+    if (isNextKey) {
+        l.push(styles.next);
+    }
+
+    return l.join(" ");
 }
